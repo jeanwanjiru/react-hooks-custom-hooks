414dc08fc7295d23533dc863fcdfc65853c007a2
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-06-15 15:53:09 -0400

    creates a react hooks custom

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,38 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import { makeEmojiList } from "../utils";
 
-function ArticlePage() {
-  // fetch data for a post
-  const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
-
-  const url = `http://localhost:4000/posts/${id}`;
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
-
-  // set the document title
-  const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
-  useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
-
-  if (!isLoaded) return <h3>Loading...</h3>;
-
-  const { minutes, title, date, preview } = post;
+function ArticlePreview({
+  id,
+  title,
+  date = "January 1, 1970",
+  preview,
+  minutes,
+}) {
   const emojis = makeEmojiList(minutes);
 
   return (
     <article>
-      <h3>{title}</h3>
+      <h3>
+        <Link to={`/articles/${id}`}>{title}</Link>
+      </h3>
       <small>
         {date} • {emojis} {minutes} min read
       </small>
@@ -41,4 +24,4 @@ function ArticlePage() {
   );
 }
 
-export default ArticlePage;
+export default ArticlePreview;
